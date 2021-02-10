9eacb907368b72481e2373b47e854f97e8d6a89d
Amirhossein Meydani <amirhwsin@outlook.com>
2021-02-10 19:30:56 +0330

    Added routers

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,43 @@ import styled from "styled-components";
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   return (
-    <Container>
-      <Wrapper>
-        <Header />
-        <Home />
-        <Footer />
-      </Wrapper>
-    </Container>
-  )
+    <Router>
+      <div>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            
+          </ul>
+        </nav> */}
+  
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <RouteToAbout />
+          </Route>
+          
+          <Route path="/">
+            <RouteToHome />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 const Container = styled.div`
@@ -38,3 +63,19 @@ const Wrapper = styled.div`
 `
 
 export default App
+
+function RouteToHome() {
+  return (
+    <Container>
+      <Wrapper>
+        <Header />
+        <Home />
+        <Footer />
+      </Wrapper>
+    </Container>
+  )
+}
+
+function RouteToAbout() {
+  return <h1>Hi</h1>;
+}
