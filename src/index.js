2b44e161c6908a45cb43e1de484788a71124ce50
Amirhossein Meydani <amirhwsin@outlook.com>
2021-02-22 00:10:36 +0330

    Added cowsay

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+var cowsay = require("cowsay-browser");
+
+console.log(cowsay.say({
+	text : "Black https://github.com/amireshoon/black",
+	e : "^^",
+	T : "U "
+}));
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
